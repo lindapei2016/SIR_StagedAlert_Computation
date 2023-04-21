cf004ba11f51d4272c24bbd518f6c762f7847f7f
LP <linda@Lindas-MacBook-Pro.local>
2023-04-21 19:23:40 -0400

    Parametrization and easy plotting functionality.

---
+++ SIR_det_2stages_params.py
@@ -0,0 +1,20 @@
+time_end = 1000000
+ODE_steps = 100.0
+grid_grain = 0.001
+
+I_constraint = 0.1
+
+S_start = 1 - 0.0025
+I_start = 0.0025
+
+beta0 = 3.0 / 10.0
+tau = 10
+
+kappa1 = 0
+kappa2 = 1
+
+cost1 = 0
+cost2 = 1
+
+threshold1 = 0
+threshold2 = 1
