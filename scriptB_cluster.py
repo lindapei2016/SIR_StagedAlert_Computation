a0ec7e3150ebff749b91519bfdef770c78200132
lindapei2016 <lindapei2016@gmail.com>
2023-05-07 16:59:18 -0400

    Incorporating asymmetric lockdowns, max lockdown budget, and allowing threshold_down to be greater than threshold_up. Plus comments.

--- scriptB_cluster.py
+++ scriptB_cluster.py
@@ -34,8 +34,20 @@ eps = 1e-6
 
 problem = SIR.ProblemInstance()
 policies = SIR.ProblemInstance.thresholds_generator((0, problem.I_constraint + eps, problem.grid_grain),
-                                                    (0, problem.I_constraint + eps, problem.grid_grain),
+                                                    (0, 1 + eps, 0.05),
                                                     symmetric=False)
 
-problem.kappa = 1 / 100 * (rank + 1)
+# 100 processors
+problem.kappa = 1/100 * ((rank + 1) % 100)
 print(problem.find_optimum(policies, str(int(problem.kappa * 100))))
+
+# 400 processors
+# 100 values of kappa to test from 1/100 to 1
+# 4 chunks of policies to distribute
+
+# problem.kappa = 1/100 * ((rank + 1) % 100)
+# num_policies = len(policies)
+# splits = np.array_split(np.array(policies), 4)
+
+# print(problem.find_optimum(tuple(splits[(rank % 100) % 4]), str(int(problem.kappa * 100)) + "_" + str((rank % 100) % 4)))
+
