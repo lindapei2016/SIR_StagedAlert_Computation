b8cbc111bf102e7ca2dc918f914a63e435918c76
lindapei2016 <lindapei2016@gmail.com>
2023-05-07 17:00:50 -0400

    Fixed typo for early termination. Herd immunity when S < 1/R0, not >.

--- scriptB_submit.sh
+++ scriptB_submit.sh
@@ -3,8 +3,8 @@
 #$ -o SIR_det_2stages_scriptB.txt
 #$ -j y
 #$ -S /bin/bash
-#$ -pe mpi 200
+#$ -pe mpi 114
 #$ -l h_rt=240:00:00
 #$ -l h_vmem=512g
 
-mpirun -np 200 python3.6 scriptB_cluster.py "A"
+mpirun -np 114 python3.6 scriptB_cluster.py "A"
